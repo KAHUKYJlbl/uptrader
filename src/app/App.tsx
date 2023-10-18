c657598635d7b725e427b8cf3592955c437f6cc2
Iurii Pototskii <iuriipototskii2gmail.com>
2023-10-18 20:21:06 +0200

    feat: persisted store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,22 +4,25 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer, toast } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { LoadingSpinner } from '../shared/ui/loading-spinner';
 import { Oops } from '../widgets/oops';
 import { AppRouter } from './provider/router';
-import { store } from './provider/store/ui/store';
+import { persistor, store } from './provider/store/ui/store';
 
 export default function App(): JSX.Element {
   return (
     <ErrorBoundary fallback={<Oops type='error-boundary' />}>
       <Suspense fallback={<LoadingSpinner spinnerType='page' />}>
         <Provider store={store}>
-          <HelmetProvider>
-            <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
-            <RouterProvider router={AppRouter} />
-          </HelmetProvider>
+          <PersistGate loading={<LoadingSpinner spinnerType="page" />} persistor={persistor}>
+            <HelmetProvider>
+              <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
+              <RouterProvider router={AppRouter} />
+            </HelmetProvider>
+          </PersistGate>
         </Provider>
       </Suspense>
     </ErrorBoundary>
--- src/app/provider/store/ui/store.ts
+++ src/app/provider/store/ui/store.ts
@@ -1,7 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
 
 import { rootReducer } from '../model/root-reducer';
+// import { NameSpace } from '../lib/name-space';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // whitelist: [NameSpace.Projects]
+};
+
+const persistedReducer = persistReducer( persistConfig, rootReducer );
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore( store );
